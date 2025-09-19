ee4bb2c1904f16ba51bdf5dce4bd0003320bb47a
michael-helium <147859650+michael-helium@users.noreply.github.com>
2025-09-19 12:48:14 -0700

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// IMPORTANT: base must match your repo name exactly (case-sensitive)
 export default defineConfig({
+  base: "/YOINK/",
   plugins: [react()],
-  server: {
-    port: 5173
-  }
 });
